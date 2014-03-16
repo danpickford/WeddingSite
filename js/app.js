3eb42a7dc275efb53c25b1846bd890e5b129dfb7
harrisonmeister <harrisonmeister@gmail.com>
2014-03-16 19:29:17 +0000

    Added more content

--- js/app.js
+++ js/app.js
@@ -1,34 +1,45 @@
-﻿$(function() {
-	init();
+﻿$(function () {
+    init();
+
+    $(".reallink").click(function (e) {
+        window.open($(this).prop('href'), '_blank');
+    });
+
+    $('.fancybox-media').fancybox({
+        openEffect: 'none',
+        closeEffect: 'none',
+        helpers: {
+            media: {}
+        }
+    });
 });
 
-function init()
-{
-	slide = $('.slide');
-	button = $('.arrow');
-	mywindow = $(window);
-	htmlbody = $('html,body');
-	dataslide = 1;
-	//Setup waypoints plugin
-	slide.waypoint(function (event) {
-	    //cache the variable of the data-slide attribute associated with each slide
-	    dataslide = $(this).attr('data-slide');
-	    if (event === 'up') {
-	        if (dataslide - 1 == 0) {
-	            dataslide = 1;
-	        } else {
-	            dataslide = dataslide - 1;
-	        }
-	    }
-	});
+function init() {
+    slide = $('.slide');
+    button = $('.arrow');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+    dataslide = 1;
+    //Setup waypoints plugin
+    slide.waypoint(function (event) {
+        //cache the variable of the data-slide attribute associated with each slide
+        dataslide = $(this).attr('data-slide');
+        if (event === 'up') {
+            if (dataslide - 1 == 0) {
+                dataslide = 1;
+            } else {
+                dataslide = dataslide - 1;
+            }
+        }
+    });
 
-	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. 
-	function goToByScroll(dataslide, mstime) {
-	    $('html,body').animate({
-	        scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
-	    }, mstime);
-	    setActivePage(dataslide);
-	}
+    //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. 
+    function goToByScroll(dataslide, mstime) {
+        $('html,body').animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, mstime);
+        setActivePage(dataslide);
+    }
 
     //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
     button.click(function (e) {
@@ -37,16 +48,15 @@ function init()
         goToByScroll(dataslide, 2000);
     });
 
-    $('.data-slide-link').click(function(e) {
-		e.preventDefault();
-		dataslide = $(this).attr('data-slide');
-		goToByScroll(dataslide, 2000);
+    $('.data-slide-link').click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide, 2000);
     });
 }
 
-function setActivePage(dataslide)
-{
+function setActivePage(dataslide) {
     $('li').removeClass('active');
-	$('.data-slide-link[data-slide="'+dataslide+'"]').parents('li:last').addClass('active');
+    $('.data-slide-link[data-slide="' + dataslide + '"]').parents('li:last').addClass('active');
 }
 
