2e12ac96f2b9a7e0fd2070312e82e6bf2f24a434
mark.harrison <harrisonmeister@gmail.com>
2014-09-25 11:05:43 +0100

    Remove rsvp link.

--- ExpressServer.js
+++ ExpressServer.js
@@ -1,34 +1,10 @@
 var express = require('express'),
     app = express();
 
-var _mg = require('mailgun');
-
 app.use(express.static(__dirname + '/'));
 app.configure(function () {
     app.use(express.bodyParser());
 });
-
-app.post("/RSVP", function (req, res, next) {
-    var currentdate = new Date();
-
-    console.log("RSVP for " + req.body.name + " date: " + currentdate);
-
-    var body = req.body.name + " has RSVP'd from kathandmarksbigday.com with the following infomation:";
-    body = body + "\n\nName: " + req.body.name;
-    body = body + "\n\nContact: " + req.body.contact;
-    body = body + "\n\nMessage: " + req.body.message;
-    body = body + "\n\nMusic: " + req.body.music;
-    body = body + "\n\nDietary Requirements: " + req.body.diet;
-
-    var mg = new _mg.Mailgun('key-453l9v4j7iwlj9szikhpsf56bl-zjbj1');
-    mg.sendText('rsvp@markandkathsbigday.co.uk', ['kathryncrowle@hotmail.com', 'harrisonmeister@gmail.com'],
-             req.body.name + ' has RSVPd!', body,
-         { 'X-Campaign-Id': 'weddingRSVP' },
-         function (err) { err && console.log(err); });
-
-    res.send(200);
-});
-
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
